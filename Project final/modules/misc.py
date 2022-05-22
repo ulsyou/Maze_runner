cd2620f37f1e950649292455928831089a7725b6
ulsyou <ulsyou1@gmail.com>
2022-05-22 17:12:45 +0700

    Final project

--- Project final/modules/misc.py
+++ Project final/modules/misc.py
@@ -10,16 +10,16 @@ def showText(screen, font, text, color, position):
     return rect.right
 
 
-def Button(screen, position, text, font, buttoncolor=(120, 120, 120), linecolor=(20, 20, 20), textcolor=(255, 255, 255), bwidth=200, bheight=50):
+def Button(screen, position, text, font, button_color=(120, 120, 120), linecolor=(20, 20, 20), textcolor=(255, 255, 255), bwidth=200, bheight=50):
     left, top = position
     pygame.draw.line(screen, linecolor, (left, top), (left+bwidth, top), 5)
     pygame.draw.line(screen, linecolor, (left, top-2), (left, top+bheight), 5)
     pygame.draw.line(screen, linecolor, (left, top+bheight), (left+bwidth, top+bheight), 5)
     pygame.draw.line(screen, linecolor, (left+bwidth, top+bheight), (left+bwidth, top), 5)
-    pygame.draw.rect(screen, buttoncolor, (left, top, bwidth, bheight))
+    pygame.draw.rect(screen, button_color, (left, top, bwidth, bheight))
     text_render = font.render(text, 1, textcolor)
     rect = text_render.get_rect()
-    rect.centerx, rect.centery = left + bwidth / 2, top + bheight / 2
+    rect.center_x, rect.center_y = left + bwidth / 2, top + bheight / 2
     return screen.blit(text_render, rect)
 
 
